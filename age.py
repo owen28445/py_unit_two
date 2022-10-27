bfc03c6b503599cd3744c9324af1a5f4f18c2980
owen28445 <Fengji@ssfs.org>
2022-10-27 13:58:21 -0400

    did degrees and age

---
+++ age.py
@@ -0,0 +1,9 @@
+
+names = (input("What is your name"))
+
+age = int(input("how old are you"))
+
+yearOfHundred = 2022 + (100 - age)
+
+print(names, "will turn 100 in", yearOfHundred)
+
---
+++ degrees.py
@@ -0,0 +1,4 @@
+
+celsius = int(input("Enter the degrees in Celsius"))
+farenheit = ((9/5) * celsius) + 32
+print(farenheit)
